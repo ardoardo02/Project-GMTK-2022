535874ad29a5e8388322ba8918b3907c6ed16b16
Earth-chan <ardoardo02@yahoo.com>
2022-07-16 18:08:50 +0700

    Bounce Particle

--- Main.gd
+++ Main.gd
@@ -5,16 +5,21 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var EFFECT = preload("res://Scene/BounceParticle.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Daisu.connect("hit_wall", self, "on_dice_hit_wall")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func on_dice_hit_wall():
+	var effect = EFFECT.instance()
+	add_child(effect)
+	effect.global_position = Vector2($Daisu.position)
 
 func _on_Poin3_hit():
 	pass # Replace with function body.
---
+++ Script/Daisu.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+signal hit_wall
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_CollisionDetector_body_entered(body):
+	if body.is_in_group('wall'):
+		emit_signal("hit_wall")
+#	pass # Replace with function body.
