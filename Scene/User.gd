f05bedb40e08f244199233a98830b57a8e648bef
creditFool <bichlasulbulqiah@gmail.com>
2022-07-16 14:48:09 +0700

    ubah variabel daisu dan user

--- Scene/User.gd
+++ Scene/User.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 
 func _physics_process(_delta):
 	if Input.is_mouse_button_pressed(1):
-		visible = true
+		visible = false
 		get_node("CollisionShape2D").disabled = false
 		position = get_global_mouse_position()
 	else:
