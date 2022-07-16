5ecb21f05a258bf031e211095dbe13a27a337890
creditFool <bichlasulbulqiah@gmail.com>
2022-07-16 12:29:18 +0700

    mouse

--- Scene/User.gd
+++ Scene/User.gd
@@ -1,16 +1,11 @@
 extends KinematicBody2D
 
 
-var velocity = Vector2.ZERO
-
-func _physics_process(delta):
-	if Input. is_action_pressed("ui_right"):
-		position.x += 10
-	elif Input. is_action_pressed("ui_left"):
-		position.x -= 10
-	elif Input. is_action_pressed("ui_up"):
-		position.y -= 10
-	elif Input. is_action_pressed("ui_down"):
-		position.y += 10
-
-	move_and_collide(velocity)
+func _physics_process(_delta):
+	if Input.is_mouse_button_pressed(1):
+		visible = true
+		get_node("CollisionShape2D").disabled = false
+		position = get_global_mouse_position()
+	else:
+		visible = false
+		get_node("CollisionShape2D").disabled = true
