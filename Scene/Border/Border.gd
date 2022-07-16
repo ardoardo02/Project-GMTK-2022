0eefd87ea6cdcae1dbcbace9f0d6390d24de2018
Earth-chan <ardoardo02@yahoo.com>
2022-07-17 01:43:21 +0700

    6 map

--- Scene/Border/Border.gd
+++ Scene/Border/Border.gd
@@ -12,7 +12,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Spike_body_entered(body):
-	emit_signal("damaged")
--- Scene/Dot/Dot.gd
+++ Scene/Dot/Dot.gd
@@ -1,9 +1,6 @@
 extends StaticBody2D
 
 
-signal damaged
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,7 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Spike_body_entered(body):
-	emit_signal("damaged")
